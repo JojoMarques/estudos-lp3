c7fc4015cf4788683bf1fcca6820ef7b99051f27
Jossana Tavares Marques <126785824+JojoMarques@users.noreply.github.com>
2024-04-16 21:44:50 -0300

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,30 @@
+
+#EX03: Contador de Vogais e Consoantes: Peça ao usuário para 
+#digitar uma frase e retorne o número de vogais e consoantes na frase.
+
+print("Contador de vogais e consoantes:")
+frase = input("Digite uma frase:")
+
+# definição da função 
+def contagem(frase):
+    qtdVogais = 0
+    qtdConsoantes = 0
+    vogais = ["A", "E", "I", "O", "U", 
+              "À", "Á", " ", "Ã", "Ê", "É", "Í", 
+              "Ó", "Ô", "Õ"]
+    for letra in frase: # percorrendo cada letra da frase
+      if letra.isalpha():
+        if letra.upper() in vogais:
+            qtdVogais += 1
+        elif letra.isalpha():
+            qtdConsoantes += 1
+          
+    quantidades = [qtdVogais, qtdConsoantes]
+    return quantidades
+
+# chamada da função 
+resultado = contagem(frase)
+
+print("Quantidade de vogais: ", resultado[0])
+print("Quantidade de consoantes:", resultado[1])
+  
