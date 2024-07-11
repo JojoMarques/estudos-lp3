0d0aac91590da8c9e1c7c8ed442146f0220be9c3
Jossana Tavares Marques <126785824+JojoMarques@users.noreply.github.com>
2024-07-11 15:23:52 -0300

    Update calculadora_aquario.py

--- 03-modulos-pacotes/exercicios/ex01/calculadora_aquario.py
+++ 03-modulos-pacotes/exercicios/ex01/calculadora_aquario.py
@@ -5,7 +5,8 @@ def volume(comprimento, altura, largura):
 #potencia termostato
 # "De acordo com a OMS, a temperatura ambiente ideal para locais fechados varia de 23°C a 26°C."
 # fonte: https://blog.frigelar.com.br/qual-temperatura-ideal-ambiente/
-TEMP_AMBIENTE = 26.00
+# tirei a média entre os dois valores (23 e 26), e defini como temp ambiente.
+TEMP_AMBIENTE = 24.00
 def potencia_termostato(volume, temp_desejada):
     return (volume*0.05*(temp_desejada - TEMP_AMBIENTE))
 
